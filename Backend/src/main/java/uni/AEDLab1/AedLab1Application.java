8b46e133bf819c8e1c78def86418faae7ac959f7
samxdev7 <ruedasamuel02@gmail.com>
2026-08-13 22:23:22 -0600

    feat: add technology stack

---
+++ Backend/src/main/java/uni/AEDLab1/AedLab1Application.java
@@ -0,0 +1,13 @@
+package uni.AEDLab1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AedLab1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AedLab1Application.class, args);
+	}
+
+}
