ff9a1beeff19a0220db0721ee095c0e72daa06a3
garr4826 <garr4826@gmail.com>
2026-08-19 19:47:18 -0600

    feat: add ejercicio 1 student registration

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -4,10 +4,11 @@ import { ArrayMenuPanel } from './ArrayMenuPanel';
 import { OrderedArrayPanel } from './OrderedArrayPanel';
 import { DisorderedArrayPanel } from './DisorderedArrayPanel';
 import { Exercise4Panel } from './Exercise4Panel';
+import { Exercise1Panel } from './Exercise1Panel';
 
 export default function App() {
   const [currentScreen, setCurrentScreen] = useState<
-    'presentation' | 'menu' | 'ordered' | 'unordered' | 'exercise4'
+    'presentation' | 'menu' | 'ordered' | 'disordered' | 'exercise4' | 'exercise1'
   >('presentation');
 
   return (
@@ -20,7 +21,7 @@ export default function App() {
         <ArrayMenuPanel
           onBack={() => setCurrentScreen('presentation')}
           onSelectOrdered={() => setCurrentScreen('ordered')}
-          onSelectUnordered={() => setCurrentScreen('unordered')}
+          onSelectUnordered={() => setCurrentScreen('disordered')}
         />
       )}
 
@@ -37,16 +38,28 @@ export default function App() {
         />
       )}
 
-      {currentScreen === 'unordered' && (
+      {currentScreen === 'disordered' && (
         <DisorderedArrayPanel
           onBack={() => setCurrentScreen('menu')}
-          onSelectExercise={(id) => console.log(`Ejercicio Desordenado seleccionado: ${id}`)}
+          onSelectExercise={(id) => {
+            if(id === 1){
+              setCurrentScreen('exercise1');
+            } else {
+              console.log(`Ejercicio Desordenado seleccionado: ${id}`)
+            }
+          }}
         />
       )}
 
       {currentScreen === 'exercise4' && (
         <Exercise4Panel
           onBack={() => setCurrentScreen('ordered')}
+        />
+      )}
+
+      {currentScreen === 'exercise1' && (
+        <Exercise1Panel
+          onBack={() => setCurrentScreen('disordered')}
         />
       )}
     </main>
