4599d7e560efe6dd4a24988f101177950b5a25f6
samxdev7 <ruedasamuel02@gmail.com>
2026-08-30 14:16:04 -0600

    refactor: fix incorrect logic and polimorifsm structure

--- Backend/src/main/java/uni/AEDLab1/controller/AlgoritmoController.java
+++ Backend/src/main/java/uni/AEDLab1/controller/AlgoritmoController.java
@@ -3,9 +3,11 @@ package uni.AEDLab1.controller;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import uni.AEDLab1.models.AlgoritmosDto;
-import uni.AEDLab1.services.BusquedaBinaria;
-import uni.AEDLab1.services.Ordenamiento;
+import uni.AEDLab1.models.BusquedaBinariaDto;
+import uni.AEDLab1.models.OrdenamientoDto;
+import uni.AEDLab1.services.BusquedaBinariaServicio;
+import uni.AEDLab1.services.OrdenamientoServicio;
+import uni.AEDLab1.services.BusquedaBinariaServicio.ResultadoBusqueda;
 
 /**
  * Controlador REST para gestionar los algoritmos de ordenamiento y de búsqueda binaria.
@@ -16,8 +18,8 @@ import uni.AEDLab1.services.Ordenamiento;
 public class AlgoritmoController {
     
     // Instancia en memoria del servicio para el Laboratorio 2
-    private Ordenamiento servicioOrdenamiento = new Ordenamiento();
-    private BusquedaBinaria servicioBusquedaBinaria = new BusquedaBinaria();
+    private OrdenamientoServicio servicioOrdenamiento = new OrdenamientoServicio();
+    private BusquedaBinariaServicio servicioBusquedaBinaria = new BusquedaBinariaServicio();
     
     /**
      * Endpoint POST: /algoritmo/ordenamiento
@@ -25,8 +27,8 @@ public class AlgoritmoController {
      * una vez ordenado se devuelve.
      */
     @PostMapping("/algoritmo/ordenamiento")
-    public ResponseEntity<?> ordenarArreglo(@RequestBody AlgoritmosDto elementos) {
-        int[] arregloOrdenado = servicioOrdenamiento.ejecutar(elementos);
+    public ResponseEntity<?> ordenarArreglo(@RequestBody OrdenamientoDto datos) {
+        int[] arregloOrdenado = servicioOrdenamiento.ejecutar(datos);
         
         // Si no se ordena el arreglo correctamente o se selecciona un método inválido, retorna null
         if (arregloOrdenado == null) {
@@ -37,22 +39,32 @@ public class AlgoritmoController {
     }
     
     /**
-     * Endpoint GET: /algoritmo/busqueda
+     * Endpoint POST: /algoritmo/busqueda
      * Consume el elemento a encontrar y el arreglo, se devuelve el indice del elemento encontrado.
      */
-    @GetMapping("/algoritmo/busqueda")
-    public ResponseEntity<?> buscarElemento(@RequestBody AlgoritmosDto elementos) {
-        int[] arregloOrdenado = servicioOrdenamiento.ejecutar(elementos);
-        AlgoritmosDto elementosConArregloOrdenado = new AlgoritmosDto(
-            elementos.tam(), 6, arregloOrdenado, elementos.buscar());
+    @PostMapping("/algoritmo/busqueda")
+    public ResponseEntity<?> buscarElemento(@RequestBody BusquedaBinariaDto datos) {
+        // Se específica el método de ordenación "Shell" por su rapidez (opción 6)
+        OrdenamientoDto arregloOriginal = new OrdenamientoDto(datos.tam(), datos.arreglo(), 6);
         
-        int indiceElementoEncontrado = servicioBusquedaBinaria.buscarElemento(elementosConArregloOrdenado);
+        int[] arregloOrdenado = servicioOrdenamiento.ejecutar(arregloOriginal);
+        if (arregloOrdenado == null) {
+            return new ResponseEntity<>("Error: No se ordenó el arreglo con alguno de los métodos de ordenación.", HttpStatusCode.valueOf(400));
+        }
+        
+        // Luego se crea un nuevo DTO para la ejecución de servicio de Busqueda Binaria para el 
+        // arreglo ya ordenado.
+        BusquedaBinariaDto busquedaArregloOrdenado = new BusquedaBinariaDto(
+            datos.tam(), arregloOrdenado, datos.objetivo());
+        
+        int indiceElementoEncontrado = servicioBusquedaBinaria.buscarElemento(busquedaArregloOrdenado);
         
         // Si no encuentra el elemento solicitado, devuelve -1
         if (indiceElementoEncontrado == -1) {
-            return new ResponseEntity<>("Error: No se encontró el elemento " + elementos.buscar() + " dentro del arreglo.", HttpStatusCode.valueOf(400));
+            return new ResponseEntity<>("Error: No se encontró el elemento " + datos.objetivo() + " dentro del arreglo.", HttpStatusCode.valueOf(400));
         }
         
-        return new ResponseEntity<>(indiceElementoEncontrado, HttpStatusCode.valueOf(201));
+        ResultadoBusqueda res = new ResultadoBusqueda(arregloOrdenado, indiceElementoEncontrado);
+        return new ResponseEntity<>(res, HttpStatusCode.valueOf(201));
     }
 }
--- Backend/src/main/java/uni/AEDLab1/services/Algoritmo.java
+++
@@ -1,8 +0,0 @@
-package uni.AEDLab1.services;
-
-import uni.AEDLab1.models.AlgoritmosDto;
-
-public abstract class Algoritmo {
-    public abstract int[] ordenar(AlgoritmosDto entradas);
-    public abstract int buscarElemento(AlgoritmosDto entradas);
-}
