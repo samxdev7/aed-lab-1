4f82b05c0589b08cda0b8d169779cca73249f1a7
garr4826 <garr4826@gmail.com>
2026-08-29 22:31:06 -0600

    feat: add binary search method

--- Backend/src/main/java/uni/AEDLab1/services/BusquedaBinaria.java
+++ Backend/src/main/java/uni/AEDLab1/services/BusquedaBinaria.java
@@ -1,6 +1,7 @@
 package uni.AEDLab1.services;
 
 import uni.AEDLab1.models.AlgoritmosDto;
+import javax.swing.JOptionPane;
 
 public class BusquedaBinaria extends Algoritmo {
     
@@ -8,5 +9,30 @@ public class BusquedaBinaria extends Algoritmo {
     public int[] ordenar(AlgoritmosDto entradas) { return null; }
 
     @Override
-    public int[] buscarElemento(AlgoritmosDto entradas) { return null; }
+    public int[] buscarElemento(AlgoritmosDto entradas) { 
+        int tam = entradas.tam();
+        int[] elementos = entradas.elementos();
+        int x = entradas.buscar();
+        
+        //Algoritmo de busqueda binaria
+        int inicio = 0, fin = tam - 1;
+        boolean Band = false;
+        while((inicio <= fin) && (Band == false)){
+            int centro = (int) (inicio + fin)/2;
+            if(x == elementos[centro]){
+                Band = true;
+            }else if (x < elementos[centro]){
+                    fin--;
+                } else{
+                   inicio++; 
+                }   
+            }
+    
+        if(Band == true){
+            JOptionPane.showMessageDialog(null, x +" se encuentra en la posición " + centro);
+        }else {
+            JOptionPane.showMessageDialog(null, x +" no se encuentra en el arreglo.");
+
+        }
+    }
 }
